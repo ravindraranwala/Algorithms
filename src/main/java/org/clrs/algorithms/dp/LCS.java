a632444b7fa382d76515fd92e840bf69b2d5a4e4
Ravindra <ravindra.ranwala@pearson.com>
2020-02-13 22:14:36 +0530

    longest common subsequence problem was completed.

---
+++ src/main/java/org/clrs/algorithms/dp/LCS.java
@@ -0,0 +1,52 @@
+package org.clrs.algorithms.dp;
+
+public final class LCS {
+
+	public static void main(String[] args) {
+//		final String seqOne = "ABCBDAB";
+//		final String seqTwo = "BDCABA";
+
+		final String strandOne = "ACCGGTCGAGTGCGCGGAAGCCGGCCGAA";
+		final String strandTwo = "GTCGTTCGGAATGCCGTTGCTCTGTAAA";
+		int[][] c = lcsLength(strandOne, strandTwo);
+		System.out.println(String.format("The length of an LCS of two sequences is: %d",
+				c[strandOne.length()][strandTwo.length()]));
+		printLcs(c, strandOne, strandTwo, strandOne.length(), strandTwo.length());
+	}
+
+	private static void printLcs(int[][] c, String x, String y, int i, int j) {
+		if (i == 0 || j == 0)
+			System.out.print("");
+		else if (x.charAt(i - 1) == y.charAt(j - 1)) {
+			printLcs(c, x, y, i - 1, j - 1);
+			System.out.print(x.charAt(i - 1));
+		} else if (c[i - 1][j] >= c[i][j - 1])
+			printLcs(c, x, y, i - 1, j);
+		else
+			printLcs(c, x, y, i, j - 1);
+
+	}
+
+	private static int[][] lcsLength(String x, String y) {
+		final int m = x.length();
+		final int n = y.length();
+		final int[][] c = new int[m + 1][n + 1];
+
+		for (int i = 1; i <= m; i++)
+			c[i][0] = 0;
+		for (int j = 0; j <= n; j++)
+			c[0][j] = 0;
+
+		for (int i = 1; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (x.charAt(i - 1) == y.charAt(j - 1))
+					c[i][j] = c[i - 1][j - 1] + 1;
+				else if (c[i - 1][j] >= c[i][j - 1])
+					c[i][j] = c[i - 1][j];
+				else
+					c[i][j] = c[i][j - 1];
+			}
+		}
+		return c;
+	}
+}
