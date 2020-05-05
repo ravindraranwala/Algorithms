d3ac3038395b6a518238aa2695ec14dcd580d624
Ravindra <ravindra.ranwala@pearson.com>
2020-05-05 21:49:57 +0530

    Edit distance print transformations and alignments.

--- src/main/java/org/clrs/algorithms/dp/EditDistance.java
+++ src/main/java/org/clrs/algorithms/dp/EditDistance.java
@@ -1,12 +1,24 @@
 package org.clrs.algorithms.dp;
 
+import static org.clrs.algorithms.dp.EditDistance.TRANSFORMATION.*;
+
 public final class EditDistance {
 	public static void main(String[] args) {
-		int minDist = editDistance("intention", "execution");
+		// int minDist = editDistance("mart", "karma");
+		String x = "intention";
+		String y = "execution";
+		int[][] c = editDistance(x, y);
+		int minDist = c[x.length()][y.length()];
 		System.out.println(String.format("Min Edit Distance: %d", minDist));
+		StringBuilder alignOne = new StringBuilder();
+		StringBuilder alignTwo = new StringBuilder();
+		printAlignment(c, x, y, x.length(), y.length(), alignOne, alignTwo);
+		System.out.println();
+		System.out.println(alignOne);
+		System.out.println(alignTwo);
 	}
 
-	private static int editDistance(String x, String y) {
+	private static int[][] editDistance(String x, String y) {
 		final int m = x.length();
 		final int n = y.length();
 		final int[][] c = new int[m + 1][n + 1];
@@ -26,10 +38,83 @@ public final class EditDistance {
 					c[i][j] = Math.min(Math.min(c[i - 1][j], c[i][j - 1]), c[i - 1][j - 1]) + 1;
 			}
 		}
-		return c[m][n];
+		return c;
+	}
+
+	private static void printAlignment(int[][] c, String x, String y, int i, int j, StringBuilder alignOne,
+			StringBuilder alignTwo) {
+		// Base case of our recursion
+		if (i == 0 && j == 0)
+			return;
+		
+		int minCost = Integer.MAX_VALUE, iPrev = -1, jPrev = -1;
+		TRANSFORMATION t = null;
+		/*
+		 * Note that COPY should be given highest priority and treated first when
+		 * confronted with ties, since it is the most optimal operation.
+		 */
+		// COPY or REPLACE operation
+		if (i > 0 && j > 0 && c[i - 1][j - 1] < minCost) {
+			iPrev = i - 1;
+			jPrev = j - 1;
+			minCost = c[i - 1][j - 1];
+			if (x.charAt(i - 1) != y.charAt(j - 1))
+				t = REPLACE;
+			else
+				t = COPY;
+		}
+		// Assume DELETE is the optimal operation
+		if (i > 0 && c[i - 1][j] < minCost) {
+			iPrev = i - 1;
+			jPrev = j;
+			minCost = c[i - 1][j];
+			t = DELETE;
+		}
+		// INSERT is the optimal operation so far
+		if (j > 0 && c[i][j - 1] < minCost) {
+			minCost = c[i][j - 1];
+			t = INSERT;
+			iPrev = i;
+			jPrev = j - 1;
+		}
+
+		printAlignment(c, x, y, iPrev, jPrev, alignOne, alignTwo);
+		t.apply(alignOne, alignTwo, x.charAt(i - 1), y.charAt(j - 1));
+		System.out.print(t + (i == x.length() && j == y.length() ? "" : ", "));
 	}
 
 	static enum TRANSFORMATION {
-		COPY, REPLACE, INSERT, DELETE;
+		COPY {
+			@Override
+			void apply(StringBuilder alignOne, StringBuilder alignTwo, char source, char target) {
+				alignOne.append(source);
+				alignTwo.append(target);
+			}
+		},
+		REPLACE {
+			@Override
+			void apply(StringBuilder alignOne, StringBuilder alignTwo, char source, char target) {
+				alignOne.append(source);
+				alignTwo.append(target);
+			}
+		},
+		INSERT {
+			@Override
+			void apply(StringBuilder alignOne, StringBuilder alignTwo, char source, char target) {
+				alignOne.append(PLACE_HOLDER);
+				alignTwo.append(target);
+			}
+		},
+		DELETE {
+			@Override
+			void apply(StringBuilder alignOne, StringBuilder alignTwo, char source, char target) {
+				alignOne.append(source);
+				alignTwo.append(PLACE_HOLDER);
+			}
+		};
+
+		private static final String PLACE_HOLDER = "_";
+
+		abstract void apply(StringBuilder alignOne, StringBuilder alignTwo, char source, char target);
 	}
 }
