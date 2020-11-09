48d92a1e59897ae8639961da48b3f2300a6863de
Ravindra <ravindra.ranwala@pearson.com>
2020-11-09 08:29:47 +0530

    square matrix multiply recursive procedure was added.

--- src/main/java/org/clrs/algorithms/dc/MatrixMultiply.java
+++ src/main/java/org/clrs/algorithms/dc/MatrixMultiply.java
@@ -23,6 +23,16 @@ public class MatrixMultiply {
 		final int[][] s = new int[][] { { 1, 2, 3 } };
 		int[][] t = matrixMultiply(r, s);
 		System.out.println(Arrays.deepToString(t));
+
+		final int[][] m4 = new int[][] { { 5, 8 }, { 3, 8 } };
+		final int[][] m5 = new int[][] { { 3, 8 }, { 8, 9 } };
+		int[][] m6 = squareMatrixMulRec(m4, m5);
+		System.out.println(Arrays.deepToString(m6));
+
+		final int[][] m7 = new int[][] { { 5, 7, 9, 10 }, { 2, 3, 3, 8 }, { 8, 10, 2, 3 }, { 3, 3, 4, 8 } };
+		final int[][] m8 = new int[][] { { 3, 10, 12, 18 }, { 12, 1, 4, 9 }, { 9, 10, 12, 2 }, { 3, 12, 4, 10 } };
+		int[][] m9 = squareMatrixMulRec(m7, m8);
+		System.out.println(Arrays.deepToString(m9));
 	}
 
 	public static int[][] matrixMultiply(int[][] a, int[][] b) {
@@ -50,6 +60,40 @@ public class MatrixMultiply {
 				for (int k = 0; k < a[0].length; k++)
 					c[i][j] += a[i][k] * b[k][j];
 
+		return c;
+	}
+
+	private static void squareMatrixMultiplyRecursive(int[][] a, int[][] b, int startRowA, int startColA, int startRowB,
+			int startColB, int n, int[][] c) {
+		if (n == 1)
+			// Note the combine step here.
+			c[startRowA][startColB] += a[startRowA][startColA] * b[startRowB][startColB];
+		else {
+			// partition A, B and C.
+			final int mid = n / 2;
+			squareMatrixMultiplyRecursive(a, b, startRowA, startColA, startRowB, startColB, mid, c);
+			squareMatrixMultiplyRecursive(a, b, startRowA, startColA + mid, startRowB + mid, startColB, mid, c);
+
+			squareMatrixMultiplyRecursive(a, b, startRowA, startColA, startRowB, startColB + mid, mid, c);
+			squareMatrixMultiplyRecursive(a, b, startRowA, startColA + mid, startRowB + mid, startColB + mid, mid, c);
+
+			squareMatrixMultiplyRecursive(a, b, startRowA + mid, startColA, startRowB, startColB, mid, c);
+			squareMatrixMultiplyRecursive(a, b, startRowA + mid, startColA + mid, startRowB + mid, startColB, mid, c);
+
+			squareMatrixMultiplyRecursive(a, b, startRowA + mid, startColA, startRowB, startColB + mid, mid, c);
+			squareMatrixMultiplyRecursive(a, b, startRowA + mid, startColA + mid, startRowB + mid, startColB + mid, mid,
+					c);
+		}
+	}
+
+	public static int[][] squareMatrixMulRec(int[][] a, int[][] b) {
+		final int n = a.length;
+		final int[][] c = new int[n][n];
+		// initialize the matrix to zero.
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				c[i][j] = 0;
+		squareMatrixMultiplyRecursive(a, b, 0, 0, 0, 0, n, c);
 		return c;
 	}
 }
