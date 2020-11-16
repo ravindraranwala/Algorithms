6449b8fe3e603f7633bb6185286de3b28495678e
Ravindra <ravindra.ranwala@pearson.com>
2020-11-16 15:20:38 +0530

    modify Strassen’s algorithm to multiply n x n matrices in which n is not an exact power of 2.

--- src/main/java/org/clrs/algorithms/dc/Strassens.java
+++ src/main/java/org/clrs/algorithms/dc/Strassens.java
@@ -21,26 +21,48 @@ public class Strassens {
 		int[][] m2 = squareMatrixMultiply(d, 0, n, e, n, 0, n);
 		int[][] m3 = matrixSum(m1, m2, Integer::sum);
 		System.out.println(Arrays.deepToString(m3));
+
+		/*
+		 * How would you modify Strassen’s algorithm to multiply n x n matrices in which
+		 * n is not an exact power of 2?
+		 */
+		final int[][] f = { { 2, 7, 3 }, { 1, 5, 8 }, { 0, 4, 1 } };
+		final int[][] g = { { 3, 0, 1 }, { 2, 1, 0 }, { 1, 2, 4 } };
+		final int[][] m4 = squareMatrixMultiply(f, g);
+		System.out.println(Arrays.deepToString(m4));
 	}
 
 	public static int[][] squareMatrixMultiply(int[][] a, int[][] b) {
-		if (a.length == 0 || a.length != a[0].length || a.length != b.length || a[0].length != b[0].length)
+		int n = a.length;
+		if (n == 0 || n != a[0].length || n != b.length || a[0].length != b[0].length)
 			throw new IllegalArgumentException(
 					"Not conformable for multiplication, different dimensions or empty matrices provided.");
 
-		return squareMatrixMultiply(a, 0, 0, b, 0, 0, a.length);
+		if (isExactPowerOf2(n)) {
+			return squareMatrixMultiply(a, 0, 0, b, 0, 0, n);
+		} else {
+			final int m = (int) Math.pow(2, Math.ceil(base2Exponent(n)));
+			final int[][] paddedA = new int[m][m];
+			final int[][] paddedB = new int[m][m];
+			matrixCopy(a, paddedA, 0, 0);
+			matrixCopy(b, paddedB, 0, 0);
+			int[][] paddedC = squareMatrixMultiply(paddedA, 0, 0, paddedB, 0, 0, m);
+			final int[][] c = new int[n][n];
+			matrixCopy(paddedC, c, 0, 0, n);
+			return c;
+		}
 	}
 
 	private static int[][] squareMatrixMultiply(int[][] a, int startRowA, int startColA, int[][] b, int startRowB,
-			int startColB, int n) {
-		if (!isExactPowerOf2(n))
-			throw new IllegalArgumentException(String.format("n = %d should be an exact power of 2", n));
+			int startColB, int side) {
+		if (!isExactPowerOf2(side))
+			throw new IllegalArgumentException(String.format("n = %d should be an exact power of 2", side));
 		// Base case of the recursion.
-		if (n == 1)
+		if (side == 1)
 			return new int[][] { { a[startRowA][startColA] * b[startRowB][startColB] } };
 
 		// partition A, B and C.
-		final int mid = n / 2;
+		final int mid = side / 2;
 		final int[][] s1 = matrixSum(b, startRowB, startColB + mid, b, startRowB + mid, startColB + mid, mid,
 				(x, y) -> x - y);
 		final int[][] s2 = matrixSum(a, startRowA, startColA, a, startRowA, startColA + mid, mid, Integer::sum);
@@ -72,7 +94,7 @@ public class Strassens {
 		final int[][] c22 = matrixSum(matrixSum(p5, p3, (x, y) -> x - y), matrixSum(p1, p7, (x, y) -> x - y),
 				Integer::sum);
 
-		final int[][] c = new int[n][n];
+		final int[][] c = new int[side][side];
 		matrixCopy(c11, c, 0, 0);
 		matrixCopy(c12, c, 0, mid);
 		matrixCopy(c21, c, mid, 0);
@@ -89,11 +111,11 @@ public class Strassens {
 	}
 
 	private static int[][] matrixSum(int[][] a, int startRowA, int startColA, int[][] b, int startRowB, int startColB,
-			int n, IntBinaryOperator binOp) {
-		final int[][] c = new int[n][n];
+			int side, IntBinaryOperator binOp) {
+		final int[][] c = new int[side][side];
 		// raw major order.
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++)
+		for (int i = 0; i < side; i++)
+			for (int j = 0; j < side; j++)
 				c[i][j] = binOp.applyAsInt(a[startRowA + i][startColA + j], b[startRowB + i][startColB + j]);
 		return c;
 	}
@@ -106,8 +128,15 @@ public class Strassens {
 	}
 
 	private static void matrixCopy(int[][] source, int[][] target, int startRow, int startCol) {
-		for (int i = 0; i < source.length; i++)
-			for (int j = 0; j < source[0].length; j++)
+		matrixCopy(source, target, startRow, startCol, source.length);
+	}
+
+	private static void matrixCopy(int[][] source, int[][] target, int startRow, int startCol, int side) {
+		if (side > target.length)
+			throw new IllegalArgumentException("Target matrix is too smaller than number of elements to be copied !");
+
+		for (int i = 0; i < side; i++)
+			for (int j = 0; j < side; j++)
 				target[startRow + i][startCol + j] = source[i][j];
 	}
 }
