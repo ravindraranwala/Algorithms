677a3fd0eaa51d2be6f56d2f20545b872bb05df3
Ravindra Ranwala <ravindraranwala@Ravindras-MacBook-Pro.local>
2021-07-03 17:45:17 +0530

    k-way merge code refactoring.

--- src/main/java/org/clrs/algorithms/heaps/K_WayMerge.java
+++ src/main/java/org/clrs/algorithms/heaps/K_WayMerge.java
@@ -4,8 +4,8 @@ import java.lang.reflect.Array;
 import java.util.AbstractMap;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.Map;
-import java.util.NoSuchElementException;
 
 public class K_WayMerge {
 	private K_WayMerge() {
@@ -18,42 +18,35 @@ public class K_WayMerge {
 		final Integer[] sortedThree = { 3, 6, 21 };
 		final Integer[] sortedFour = { 4, 8, 9 };
 
-		final Integer[] mergedData = kWayMerge(sortedOne, sortedTwo, sortedThree, sortedFour);
+		final Integer[] mergedData = kWayMerge(Integer.class, sortedOne, sortedTwo, sortedThree, sortedFour);
 		System.out.println(Arrays.toString(mergedData));
 	}
 
-	public static <T> T[] kWayMerge(Comparator<? super T> cmp, T[]... sortedLists) {
-		final int k = sortedLists.length;
-		final Queue<Map.Entry<T, Integer>> queue = PriorityQueue.of(Map.Entry.comparingByKey(cmp), k);
+	public static <T> T[] kWayMerge(Comparator<? super T> cmp, Class<T> clazz, T[]... sortedLists) {
+		final Queue<Map.Entry<T, Iterator<T>>> queue = PriorityQueue.of(Map.Entry.comparingByKey(cmp),
+				sortedLists.length);
 		int n = 0;
-		int pos = 0;
-		final int[] nextIdx = new int[k];
 		for (T[] a : sortedLists) {
-			if (a.length == 0)
-				throw new NoSuchElementException("Empty array is provided !");
-			// Adding the first element of each list to the heap.
-			nextIdx[pos] = 1;
-			queue.insert(new AbstractMap.SimpleEntry<>(a[0], pos++));
+			final Iterator<T> it = Arrays.asList(a).iterator();
+			if (it.hasNext())
+				queue.insert(new AbstractMap.SimpleEntry<>(it.next(), it));
 			n += a.length;
 		}
 
 		@SuppressWarnings("unchecked")
-		final T[] sortedData = (T[]) Array.newInstance(sortedLists[0].getClass().getComponentType(), n);
+		final T[] sortedData = (T[]) Array.newInstance(clazz, n);
 		int count = 0;
 		while (!queue.isEmpty()) {
-			final Map.Entry<T, Integer> entry = queue.extract();
+			final Map.Entry<T, Iterator<T>> entry = queue.extract();
 			sortedData[count++] = entry.getKey();
-			final int listPos = entry.getValue();
-			final int nextArrIdx = nextIdx[listPos];
-			if (nextArrIdx < sortedLists[listPos].length) {
-				queue.insert(new AbstractMap.SimpleEntry<>(sortedLists[listPos][nextArrIdx], listPos));
-				nextIdx[listPos] += 1;
-			}
+			final Iterator<T> it = entry.getValue();
+			if (it.hasNext())
+				queue.insert(new AbstractMap.SimpleEntry<>(it.next(), it));
 		}
 		return sortedData;
 	}
 
-	public static <T extends Comparable<? super T>> T[] kWayMerge(T[]... sortedLists) {
-		return kWayMerge(Comparator.naturalOrder(), sortedLists);
+	public static <T extends Comparable<? super T>> T[] kWayMerge(Class<T> clazz, T[]... sortedLists) {
+		return kWayMerge(Comparator.naturalOrder(), clazz, sortedLists);
 	}
 }
