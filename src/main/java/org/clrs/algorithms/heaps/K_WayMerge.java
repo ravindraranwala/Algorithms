fec87de0cdf58b4bcaf31b284bacd0c6be2a8ad8
Ravindra <ravindra.ranwala@pearson.com>
2021-07-03 08:25:55 +0530

    K-way merge code refactoring.

--- src/main/java/org/clrs/algorithms/heaps/K_WayMerge.java
+++ src/main/java/org/clrs/algorithms/heaps/K_WayMerge.java
@@ -1,8 +1,10 @@
 package org.clrs.algorithms.heaps;
 
 import java.lang.reflect.Array;
+import java.util.AbstractMap;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class K_WayMerge {
@@ -22,7 +24,7 @@ public class K_WayMerge {
 
 	public static <T> T[] kWayMerge(Comparator<? super T> cmp, T[]... sortedLists) {
 		final int k = sortedLists.length;
-		final Queue<ElementWrapper<T>> queue = PriorityQueue.of(new ElementWrapperComparator<T>(cmp), k);
+		final Queue<Map.Entry<T, Integer>> queue = PriorityQueue.of(Map.Entry.comparingByKey(cmp), k);
 		int n = 0;
 		int pos = 0;
 		final int[] nextIdx = new int[k];
@@ -31,7 +33,7 @@ public class K_WayMerge {
 				throw new NoSuchElementException("Empty array is provided !");
 			// Adding the first element of each list to the heap.
 			nextIdx[pos] = 1;
-			queue.insert(new ElementWrapper<>(a[0], pos++));
+			queue.insert(new AbstractMap.SimpleEntry<>(a[0], pos++));
 			n += a.length;
 		}
 
@@ -39,12 +41,13 @@ public class K_WayMerge {
 		final T[] sortedData = (T[]) Array.newInstance(sortedLists[0].getClass().getComponentType(), n);
 		int count = 0;
 		while (!queue.isEmpty()) {
-			final ElementWrapper<T> wrapper = queue.extract();
-			sortedData[count++] = wrapper.elt;
-			final int nextArrIdx = nextIdx[wrapper.pos];
-			if (nextArrIdx < sortedLists[wrapper.pos].length) {
-				queue.insert(new ElementWrapper<T>(sortedLists[wrapper.pos][nextArrIdx], wrapper.pos));
-				nextIdx[wrapper.pos] += 1;
+			final Map.Entry<T, Integer> entry = queue.extract();
+			sortedData[count++] = entry.getKey();
+			final int listPos = entry.getValue();
+			final int nextArrIdx = nextIdx[listPos];
+			if (nextArrIdx < sortedLists[listPos].length) {
+				queue.insert(new AbstractMap.SimpleEntry<>(sortedLists[listPos][nextArrIdx], listPos));
+				nextIdx[listPos] += 1;
 			}
 		}
 		return sortedData;
@@ -52,28 +55,5 @@ public class K_WayMerge {
 
 	public static <T extends Comparable<? super T>> T[] kWayMerge(T[]... sortedLists) {
 		return kWayMerge(Comparator.naturalOrder(), sortedLists);
-	}
-
-	static class ElementWrapper<E> {
-		final E elt;
-		final int pos;
-
-		ElementWrapper(E elt, int pos) {
-			this.elt = elt;
-			this.pos = pos;
-		}
-	}
-
-	static class ElementWrapperComparator<E> implements Comparator<ElementWrapper<E>> {
-		private final Comparator<? super E> cmp;
-
-		ElementWrapperComparator(Comparator<? super E> cmp) {
-			this.cmp = cmp;
-		}
-
-		@Override
-		public int compare(ElementWrapper<E> o1, ElementWrapper<E> o2) {
-			return this.cmp.compare(o1.elt, o2.elt);
-		}
 	}
 }
