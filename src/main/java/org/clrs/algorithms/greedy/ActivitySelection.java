e229e67ccf2a914a7cae325bf5272fa55003b744
Ravindra <ravindra.ranwala@pearson.com>
2021-08-22 13:57:45 +0530

    recursive activity selector code refactoring.

--- src/main/java/org/clrs/algorithms/greedy/ActivitySelection.java
+++ src/main/java/org/clrs/algorithms/greedy/ActivitySelection.java
@@ -1,6 +1,5 @@
 package org.clrs.algorithms.greedy;
 
-import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -13,23 +12,24 @@ class ActivitySelection {
 	public static void main(String[] args) {
 		final int[] s = { 1, 3, 0, 5, 3, 5, 6, 8, 8, 2, 12 };
 		final int[] f = { 4, 5, 6, 7, 9, 9, 10, 11, 12, 14, 16 };
-		final int[] a = recursiveActivitySelector(s, f, -1, 0);
-		System.out.println(Arrays.toString(a));
+		final Set<Integer> a2 = new LinkedHashSet<>();
+		recursiveActivitySelector(s, f, 0, a2);
+		System.out.println(a2);
 		final Set<Integer> a1 = greedyActivitySelector(s, f);
 		System.out.println(a1);
 	}
 
-	static int[] recursiveActivitySelector(int[] s, int[] f, int k, int i) {
+	static void recursiveActivitySelector(int[] s, int[] f, int k, Set<Integer> a) {
+		if (k == 0)
+			a.add(0);
 		int m = k + 1;
 		final int n = s.length;
-		while (m < n && m > 0 && s[m] < f[k])
+		while (m < n && s[m] < f[k])
 			m = m + 1;
 		if (m < n) {
-			final int[] a = recursiveActivitySelector(s, f, m, i + 1);
-			a[i] = m;
-			return a;
-		} else
-			return new int[i];
+			a.add(m);
+			recursiveActivitySelector(s, f, m, a);
+		}
 	}
 
 	static Set<Integer> greedyActivitySelector(int[] s, int[] f) {
