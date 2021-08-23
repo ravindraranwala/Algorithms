f7715f76ff13b53ccf680df26bd7d9469d17dbff
Ravindra <ravindra.ranwala@pearson.com>
2021-08-23 06:36:21 +0530

    recursive activity selector code refactoring.

--- src/main/java/org/clrs/algorithms/greedy/ActivitySelection.java
+++ src/main/java/org/clrs/algorithms/greedy/ActivitySelection.java
@@ -12,16 +12,20 @@ class ActivitySelection {
 	public static void main(String[] args) {
 		final int[] s = { 1, 3, 0, 5, 3, 5, 6, 8, 8, 2, 12 };
 		final int[] f = { 4, 5, 6, 7, 9, 9, 10, 11, 12, 14, 16 };
-		final Set<Integer> a2 = new LinkedHashSet<>();
-		recursiveActivitySelector(s, f, 0, a2);
-		System.out.println(a2);
+		final Set<Integer> a = recursiveActivitySelector(s, f);
+		System.out.println(a);
 		final Set<Integer> a1 = greedyActivitySelector(s, f);
 		System.out.println(a1);
 	}
 
-	static void recursiveActivitySelector(int[] s, int[] f, int k, Set<Integer> a) {
-		if (k == 0)
-			a.add(0);
+	static Set<Integer> recursiveActivitySelector(int[] s, int[] f) {
+		final Set<Integer> a = new LinkedHashSet<>();
+		a.add(0);
+		recursiveActivitySelector(s, f, 0, a);
+		return a;
+	}
+
+	private static void recursiveActivitySelector(int[] s, int[] f, int k, Set<Integer> a) {
 		int m = k + 1;
 		final int n = s.length;
 		while (m < n && s[m] < f[k])
