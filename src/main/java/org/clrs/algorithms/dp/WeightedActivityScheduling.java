6092b0cf1d5254ec887c50daeaf7c0a7d9b65e76
Ravindra <ravindra.ranwala@pearson.com>
2021-09-16 17:29:31 +0530

    Exercise 16.1-5 weighted interval scheduling completed.

---
+++ src/main/java/org/clrs/algorithms/dp/WeightedActivityScheduling.java
@@ -0,0 +1,114 @@
+package org.clrs.algorithms.dp;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringJoiner;
+
+import org.clrs.algorithms.util.BinarySearch;
+
+class WeightedActivityScheduling {
+
+	WeightedActivityScheduling() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		// usecase 1.
+		final int[] s = { 1, 2, 3, 3 };
+		final int[] f = { 3, 4, 5, 6 };
+		final int[] v = { 50, 10, 40, 70 };
+		final int[] a = new int[s.length];
+		int n = s.length;
+		int[] p = schedule(s, f, v, a);
+		System.out.println(p[n - 1]);
+		String as = findActivities(v, p, a);
+		System.out.println(as);
+
+		// Usecase 2.
+		final int[] s1 = { 1, 2, 3, 4, 6 };
+		final int[] f1 = { 3, 5, 10, 6, 9 };
+		final int[] v1 = { 20, 20, 100, 70, 60 };
+		final int[] a1 = new int[s1.length];
+		p = schedule(s1, f1, v1, a1);
+		n = s1.length;
+		System.out.println(p[n - 1]);
+		as = findActivities(v1, p, a1);
+		System.out.println(as);
+
+		// Usecase 3.
+		final int[] s2 = { 1, 1, 1 };
+		final int[] f2 = { 2, 3, 4 };
+		final int[] v2 = { 5, 6, 4 };
+		final int[] a2 = new int[s2.length];
+		p = schedule(s2, f2, v2, a2);
+		n = s2.length;
+		System.out.println(p[n - 1]);
+		as = findActivities(v2, p, a2);
+		System.out.println(as);
+	}
+
+	static int[] schedule(int[] s, int[] f, int[] v, int[] an) {
+		final int n = s.length;
+		final Activity[] a = new Activity[n];
+		for (int i = 0; i < n; i++)
+			a[i] = new Activity(s[i], f[i], v[i], i + 1);
+		Arrays.sort(a, Comparator.comparingInt(ac -> ac.f));
+		for (int i = 0; i < n; i++) {
+			s[i] = a[i].s;
+			f[i] = a[i].f;
+			v[i] = a[i].v;
+			an[i] = a[i].n;
+		}
+
+		final int[] p = new int[n];
+		int l = 0;
+		p[0] = v[0];
+		for (int k = 1; k < n; k++) {
+			if (f[l] <= s[k]) {
+				p[k] = p[k - 1] + v[k];
+				l = k;
+			} else {
+				// Note that we need a closed interval here.
+				final int c = BinarySearch.predecessor(f, s[k] + 1);
+				int cv = 0;
+				if (-1 < c)
+					cv = p[c];
+				if (p[k - 1] < cv + v[k]) {
+					p[k] = cv + v[k];
+					l = k;
+				} else
+					p[k] = p[k - 1];
+			}
+		}
+		return p;
+	}
+
+	static String findActivities(int[] v, int[] p, int[] a) {
+		final int n = v.length;
+		int r = p[n - 1];
+		final StringJoiner s = new StringJoiner(", ", "[", "]");
+		for (int i = n - 2; -1 < i; i--) {
+			if (p[i] < r) {
+				s.add(String.valueOf(a[i + 1]));
+				r = r - v[i + 1];
+			}
+		}
+		if (0 < r)
+			s.add(String.valueOf(1));
+		return s.toString();
+	}
+
+	static class Activity {
+		final int s;
+		final int f;
+		final int v;
+		final int n;
+
+		Activity(int s, int f, int v, int n) {
+			this.s = s;
+			this.f = f;
+			this.v = v;
+			this.n = n;
+		}
+	}
+}
---
+++ src/main/java/org/clrs/algorithms/util/BinarySearch.java
@@ -0,0 +1,71 @@
+package org.clrs.algorithms.util;
+
+public class BinarySearch {
+
+	BinarySearch() {
+		throw new AssertionError();
+	}
+
+	public static void main(String[] args) {
+		final int[] nums = { -1, 0, 3, 5, 9, 12 };
+		final int target = 9;
+		final int idx = search(nums, target);
+		System.out.println(idx);
+		int i = successor(nums, 1);
+		System.out.println(nums[i]);
+	}
+
+	/**
+	 * Given an array of integers nums which is sorted in ascending order, and an
+	 * integer target, this function searches target in nums. If target exists, then
+	 * return its index. Otherwise, return -1.
+	 * 
+	 * @param nums   an array of integers
+	 * @param target integer target
+	 * @return index of the target if it exists in the array. Otherwise, return -1
+	 */
+	public static int search(int[] nums, int target) {
+		int l = 0;
+		int r = nums.length - 1;
+		while (l <= r) {
+			final int m = (l + r) / 2;
+			if (nums[m] < target)
+				l = m + 1;
+			else if (nums[m] > target)
+				r = m - 1;
+			else
+				return m;
+		}
+		return -1;
+	}
+
+	public static int successor(int[] nums, int target) {
+		return successor(nums, target, 0, nums.length - 1);
+	}
+
+	public static int successor(int[] nums, int target, int i, int j) {
+		int l = i;
+		int r = j + 1;
+		while (l < r) {
+			final int mid = (l + r) / 2;
+			if (nums[mid] > target)
+				r = mid;
+			else
+				l = mid + 1;
+		}
+		return r;
+	}
+
+	public static int predecessor(int[] a, int target) {
+		int l = 0;
+		int r = a.length;
+		while (l < r) {
+			final int mid = (l + r) / 2;
+			if (a[mid] < target)
+				l = mid + 1;
+			else
+				r = mid;
+		}
+		return l - 1;
+	}
+}
